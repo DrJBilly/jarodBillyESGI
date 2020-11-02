37c12f1fa94344937a4be8f6b5b925fa50d0b422
DrJBilly <jarod.billy@hotmail.com>
2020-11-02 20:38:57 +0100

    modif .html ajout fichier .php et .js non terminé

--- contact.js
+++ contact.js
@@ -1,1 +1,24 @@
-var nom = document.getElementById("nom").value;
+
+/* A TERMINER */
+
+
+var formu = document.getElementById('formulaire');
+
+formu.addEventListener('submit', function(e) { :/* caractère autorisé sur l'input text name et prenom*/
+    var myInput = document.getElementById('name');
+    var myInput = document.getElementById('prenom');
+    var gestionerreur = /^[a-zA-Z-\s]+$/;
+
+    if (myInput.value.tirm() = "") { /* gestion d'erreur */
+        var Error = document.getElementById('erreur')
+        Error.innerHTML = "Le champs Nom est requis.";
+        Error.style.color = 'red';
+        e.preventDefault();
+
+    } else if (gestionerreur.test(myInput.value) =false) {
+        var Error = document.getElementById('erreur');
+        Error.innerHTML = "Le champs doit uniquement contenir des lettre ou tiret.";
+        Error.style.color = 'red';
+        e.preventDefault();
+    }
+});
