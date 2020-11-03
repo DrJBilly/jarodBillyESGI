d18c6e535a9fff2793b816c7d69400e5b2186ba6
DrJBilly <jarod.billy@hotmail.com>
2020-11-03 11:16:24 +0100

    correction getelementbyid name  --> nom

--- contact.js
+++ contact.js
@@ -4,8 +4,8 @@
 
 var formu = document.getElementById('formulaire');
 
-formu.addEventListener('submit', function(e) { :/* caractère autorisé sur l'input text name et prenom*/
-    var myInput = document.getElementById('name');
+formu.addEventListener('submit', function(e) { ; /* caractère autorisé sur l'input text name et prenom*/
+    var myInput = document.getElementById('nom');
     var myInput = document.getElementById('prenom');
     var gestionerreur = /^[a-zA-Z-\s]+$/;
 
@@ -15,7 +15,7 @@ formu.addEventListener('submit', function(e) { :/* caractère autorisé sur l'in
         Error.style.color = 'red';
         e.preventDefault();
 
-    } else if (gestionerreur.test(myInput.value) =false) {
+    } else if (gestionerreur.test(myInput.value) = false) {
         var Error = document.getElementById('erreur');
         Error.innerHTML = "Le champs doit uniquement contenir des lettre ou tiret.";
         Error.style.color = 'red';
